60ffbaa80a2a4686de8acaffb730face16d58eaa
Rebecca Collins <rebeccacollins1007@gmail.com>
2024-07-10 11:16:54 +0100

    fleshing out

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
-// Function to play sound
 function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
     if (!audio) return;
   
     key.classList.add('playing');
-    audio.currentTime = 0; // Rewind to the start
+    audio.currentTime = 0;
     audio.play();
   }
   
-  // Function to remove the playing class after the transition ends
+
   function removeTransition(e) {
     if (e.propertyName !== 'transform') return;
     this.classList.remove('playing');
   }
   
-  // Function to handle click events
+
   function handleClick(e) {
     const keyCode = this.getAttribute('data-key');
     const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
     if (!audio) return;
   
     this.classList.add('playing');
-    audio.currentTime = 0; // Rewind to the start
+    audio.currentTime = 0; 
     audio.play();
   }
   
