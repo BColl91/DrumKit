b5feb9f0d2a14236226c6bb0424f3875b24b2c35
Rebecca Collins <rebeccacollins1007@gmail.com>
2024-07-11 13:17:53 +0100

    Adding notes and amending a strange anomaly

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
+//function playsound(e) will grab the keycode of the pressed key, the audio and key querySelector will grab the audio and dic elements that are assiociated with the keycode.
 function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
     if (!audio) return;
-  
+  //key.classlist.add(playing) will trigger the CSS transition- the key pressed will highlight green. audio.play() will play the audio linked to the key.
     key.classList.add('playing');
     audio.currentTime = 0;
     audio.play();
   }
   
-
+//This functions takes
   function removeTransition(e) {
     if (e.propertyName !== 'transform') return;
     this.classList.remove('playing');
